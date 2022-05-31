6ad9658279c98516a2fdacf60f2c756999691657
Sebastian-Griesbach <sebastian.griesbach@zoho.eu>
2022-05-31 18:59:55 +0200

    Added Docstring to Exploration Module

--- src/tc_reinforcement_learning/exploration_module.py
+++ src/tc_reinforcement_learning/exploration_module.py
@@ -8,6 +8,8 @@ from tc_utils.util import numpy_wrap_module
 
 class ExplorationModule(ABC):
     def __init__(self) -> None:
+        """Interface for exploration handling
+        """
         super().__init__()
 
     def save_checkpoint(self, save_path, prefix=""):
@@ -18,20 +20,42 @@ class ExplorationModule(ABC):
 
     @abstractmethod
     def random(self):
+        """Generates random explorations.
+        """
         ...
 
     @abstractmethod
     def explorativ(self, base):
+        """Generates noise in relation to some base. Usually adding noise to that base
+
+        Args:
+            base: base to add exploration too.
+        """
         ...
 
 class ExplorationPolicyModule(ExplorationModule):
     def __init__(self, example_policy: torch.nn.Module, device:str = "cpu") -> None:
+        """Interface for exploration handling of entire policies. 
+
+        Args:
+            example_policy (torch.nn.Module): An example policy to acquire the structure of the network.
+            device (str, optional): On which device the noise is added. Defaults to "cpu".
+        """
         super().__init__()
         self.device = device
         self.example_policy = copy.deepcopy(example_policy.to(self.device))
 
 class EpsilonNormalExplorationPolicyModule(ExplorationPolicyModule):
     def __init__(self, example_policy: torch.nn.Module, noise_std: float, epsilon: float, random_prob: float= 0., device="cpu") -> None:
+        """Exploration module that does exploration in the parameter space of the policy by adding noise to the parameters or replacing them with random parameters.
+
+        Args:
+            example_policy (torch.nn.Module): An example policy to acquire the structure of the network.
+            noise_std (float): Standard deviation of added noise.
+            epsilon (float): Probability to not add noise.
+            random_prob (float, optional): Probability to use random parameters. Defaults to 0.
+            device (str, optional): On which device the noise is added. Defaults to "cpu".
+        """
         super().__init__(example_policy = example_policy, device = device)
         self.noise_std = noise_std
         self.epsilon = epsilon
@@ -65,7 +89,13 @@ class EpsilonNormalExplorationPolicyModule(ExplorationPolicyModule):
 
 class AdaptivNormalExplorationPolicyModule(EpsilonNormalExplorationPolicyModule):
     def __init__(self, example_policy: torch.nn.Module,  epsilon: float, min_noise_std: float = 0.01, noise_scaling: float = 1., random_prob: float= 0., device="cpu") -> None:
-        #TODO min_noise_std might not be fitting for noise std, as this is also used for random policies
+        """Exploration module similar to EpsilonNormalExplorationPolicyModule. But here noise is adapted per layer by the standard deviation of the existing parameters of that layer.
+
+        Args:
+            See EpsilonNormalExplorationPolicyModule
+            min_noise_std (float, optional): Minimal noise standard deviation used if the standard deviation of a layer is lower. Defaults to 0.01.
+            noise_scaling (float, optional): Scaling factor for the standard deviations of the layers to use a noise distribution. Defaults to 1.
+        """
         super().__init__(example_policy=example_policy,
                         noise_std=min_noise_std,
                         epsilon=epsilon,
@@ -82,6 +112,12 @@ class AdaptivNormalExplorationPolicyModule(EpsilonNormalExplorationPolicyModule)
 
 class ExplorationActionModule(ExplorationModule):
     def __init__(self, noise_std: float, action_space: Space) -> None:
+        """Exploration module that adds noise to the actions of a module.
+
+        Args:
+            noise_std (float): Standard deviation of normal noise distribution.
+            action_space (Space): Relevant action space of the policy.
+        """
         super().__init__()
         self.noise_std = noise_std
         self.action_space = action_space
